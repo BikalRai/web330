76626924fbda91baf9e77df3fd97cef9d9fa70f5
Bikal Rai <bikalrai2785@gmailcom>
2023-06-27 16:16:18 +0545

    js intro

---
+++ javascript/js/jsintro.js
@@ -0,0 +1,78 @@
+// print functions:
+// alert('Programming is fun!!');
+document.write("<h1>Programming is fun</h1>");
+
+console.log('<h1>Programming is fun</h1>');
+
+console.log(typeof '<h1>Programming is fun</h1>');
+console.log(typeof "<h1>Programming is fun</h1>");
+
+console.log(2);
+console.log(2.5);
+console.log(typeof 2);
+console.log(typeof 2.6);
+
+
+console.log('');
+console.log(' ');
+console.log(typeof '');
+
+console.log(true);
+console.log(typeof false);
+
+
+console.log('----------variables----------');
+// variable declaration
+var a;
+
+// assigning value
+a = true;
+
+console.log(a);
+console.log(typeof a);
+
+// variable initialization
+let variableName = 11;
+
+//variable reassigning
+a = 'true';
+console.log('a'); // differentiate between these two a
+console.log(a);
+
+console.log('------variable declaration keywords------');
+// var keyword
+console.log('------var keyword------');
+console.log(x);
+var x = 5;
+console.log(x);
+
+var x = 10;
+console.log(x);
+
+var y = 55;
+{
+	var y = 23;
+}
+
+console.log(y);
+
+console.log('------let keyword------');
+// console.log(b);
+let b = 'welcome to the jungle';
+console.log(b);
+// let b = 'some string';
+b = 'reassigning variable'
+console.log(b);
+
+{
+	let c = 55;
+}
+
+let d;
+console.log(d);
+
+// console.log(c);
+console.log('------const keyword------');
+const e = false; // must be initialized
+
+// e = 22;
