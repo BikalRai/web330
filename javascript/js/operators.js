f099df107359a13de5eac5dbf9f9845ba1006e15
Bikal Rai <bikalrai2785@gmailcom>
2023-06-28 16:24:45 +0545

    Operators

---
+++ javascript/js/operators.js
@@ -0,0 +1,63 @@
+// Arithmetic operators
+let a = 5;
+let b = 6;
+let c = a + b;
+console.log(c) // 11
+
+c = a - b;
+console.log(c) // -1
+
+c = a * b;
+console.log(c); // 30
+
+c = a / b;
+console.log(c)
+
+c = b % a;
+console.log(c); // 1
+
+c = 2 ** b;
+console.log(c) // 64
+
+// Assignment operators
+let x = 5;
+
+x = x + 2;
+console.log(x); // 7
+
+x += 2;
+console.log(x); // 9
+
+x = x * 2;
+console.log(x); // 18
+
+x **= 2;
+console.log(x); // 324
+
+x++;
+console.log(x); // 325
+x--;
+console.log(x) // 324
+
+x++;
+x++;
+x++;
+x++;
+x++;
+
+console.log(x); // 329
+
+// comparison
+let d = 5;
+let e = 7;
+let f = '5';
+
+d > e;
+console.log(d > e) // false
+console.log(d < e) // true
+console.log(d >= e) // false
+console.log(d <= e) // true
+console.log(d == f) // true
+console.log(d != f) // false
+console.log(d === f) // false
+console.log(d !== f) // true
