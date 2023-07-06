99238ca9efea58cd273b793ecbfbb080d088783e
Bikal Rai <bikalrai2785@gmailcom>
2023-07-06 15:40:14 +0545

    exercise update

--- javascript/js/array.js
+++ javascript/js/array.js
@@ -96,3 +96,24 @@ for (let i = 1; i <= 10; i++) {
 }
 
 console.log(oddEven);
+
+// fizzbuzz
+const fizzBuzz = [];
+
+for (let i = 1; i <= 100; i++) {
+	if(i % 3 === 0 && i % 5 === 0) {
+		fizzBuzz.push('FizzBuzz');
+	} else if (i % 3 === 0) {
+		fizzBuzz.push('Fizz');
+	} else if (i % 5 === 0) {
+		fizzBuzz.push('Buzz');
+	}else {
+		fizzBuzz.push(i);
+	}
+}
+
+console.log(fizzBuzz);
+console.log('-------exercise-------')
+fizzBuzz.forEach((ele) => {
+	console.log(ele);
+})
